58d40db0b16a169d6e951cc8426532f9a6f3e9f3
MariskaS85 <mcfsnijdewind@gmail.com>
2021-03-12 15:21:52 +0000

    second cloning commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn titik.wsgi:application
---
+++ profiles/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
+from .models import UserProfile
+from .forms import UserProfileForm
+
+from checkout.models import Order
+
+
+@login_required
+def profile(request):
+    """" A view to a users profile """
+    profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated succesfully')
+        else:
+            messages.error(request, 'Profile Failed to update, please check form')
+    else: 
+        form = UserProfileForm(instance=profile)
+    orders = profile.orders.all()
+
+    template = 'profiles/profile.html'
+    context = {
+        'form': form,
+        'orders': orders,
+        'on_profile_page': True
+    }
+
+    return render(request, template, context)
+
+
+@login_required
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, (
+        f'This is your last confirmation order number{ order_number}. '
+        'A confirmation email was send on the order date.'
+    ))
+
+    template = 'checkout/checkout_succes.html'
+    context = {
+        'order': order,
+        'from_profile': True,
+    }
+
+    return render(request, template, context)
