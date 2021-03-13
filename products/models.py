6bcd3469c0de2eb4e2bcdd50ef7ecd845299b619
MariskaS85 <mcfsnijdewind@gmail.com>
2021-03-13 17:31:10 +0000

    changed css for toasts and fixed navbar toggler

--- products/models.py
+++ products/models.py
@@ -7,23 +7,26 @@ class Category(models.Model):
         verbose_name_plural = 'Categories'
 
     name = models.CharField(max_length=254)
-    friendly_name = models.CharField(max_length=254, null=True, blank=True)
-
-    def __str__(self):
-        return self.name
+    friendly_name = models.CharField(max_length=254, null=True)
 
     def get_friendy_name(self):
         return self.friendly_name
 
+    def __str__(self):
+        return self.name
+
 
 # this shows the labels in the django admin
 class Product(models.Model):
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey('Category', null=True,
+                                 blank=True,
+                                 on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    size = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    size = models.DecimalField(max_digits=6,
+                               decimal_places=2, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
