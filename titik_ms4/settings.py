eb7d92dc31cb892b186a66e782ebed0de5526056
MariskaS85 <mcfsnijdewind@gmail.com>
2021-05-07 12:51:36 +0000

    08 deployment added MaxAge v2

--- titik_ms4/settings.py
+++ titik_ms4/settings.py
@@ -184,7 +184,6 @@ if 'USE_AWS' in os.environ:
         'Expires': 'Thu, 31 Dex 2099 20:00:00 GMT',
         'CacheControle': 'max-age=94608000',
     }
-    
     # Bucket configuration
     AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
     AWS_S3_REGION_NAME = 'eu-west-1'
