620fd8a9b24e3bcb40c15162a065735f4f19b7d0
MariskaS85 <mcfsnijdewind@gmail.com>
2021-05-07 13:13:50 +0000

    08 deployment region

--- titik_ms4/settings.py
+++ titik_ms4/settings.py
@@ -186,7 +186,7 @@ if 'USE_AWS' in os.environ:
     }
     # Bucket configuration
     AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
-    AWS_S3_REGION_NAME = 'eu-west-1'
+    AWS_S3_REGION_NAME = 'eu-west-3'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
